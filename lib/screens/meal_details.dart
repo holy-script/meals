b2ac70ac62a03a839c0fe230c356bb1d0eb9c554
holy-script <palashjohricool@gmail.com>
2025-02-26 20:25:40 +0530

    added implicit animation and configured it using tweens

--- lib/screens/meal_details.dart
+++ lib/screens/meal_details.dart
@@ -19,7 +19,19 @@ class MealDetailsScreen extends ConsumerWidget {
         title: Text(meal.title),
         actions: [
           IconButton(
-            icon: Icon(isFavorite ? Icons.star : Icons.star_border),
+            icon: AnimatedSwitcher(
+              duration: Duration(milliseconds: 300),
+              transitionBuilder: (child, animation) {
+                return RotationTransition(
+                  turns: Tween<double>(begin: 0.8, end: 1).animate(animation),
+                  child: child,
+                );
+              },
+              child: Icon(
+                isFavorite ? Icons.star : Icons.star_border,
+                key: ValueKey(isFavorite),
+              ),
+            ),
             onPressed: () {
               final wasAdded = ref
                   .read(favoritesMealsProvider.notifier)
