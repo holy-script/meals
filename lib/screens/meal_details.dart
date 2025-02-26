57aeb4cb8f9fc94ca97c892b25445739905e16d8
holy-script <palashjohricool@gmail.com>
2025-02-26 19:47:47 +0530

    cleaned up more logic for filtering meals in tabs screen using filter provider, added dynamic favorite icon using a meals' favorite status

--- lib/screens/meal_details.dart
+++ lib/screens/meal_details.dart
@@ -10,12 +10,16 @@ class MealDetailsScreen extends ConsumerWidget {
 
   @override
   Widget build(BuildContext context, WidgetRef ref) {
+    final favoriteMeals = ref.watch(favoritesMealsProvider);
+
+    final isFavorite = favoriteMeals.contains(meal);
+
     return Scaffold(
       appBar: AppBar(
         title: Text(meal.title),
         actions: [
           IconButton(
-            icon: Icon(Icons.star),
+            icon: Icon(isFavorite ? Icons.star : Icons.star_border),
             onPressed: () {
               final wasAdded = ref
                   .read(favoritesMealsProvider.notifier)
